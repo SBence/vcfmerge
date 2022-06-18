a7de80afd7aabc752d313509d78512f38a73390c
SBence <43538202+SBence@users.noreply.github.com>
2022-06-18 21:20:03 +0200

    Add main.py

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from fileinput import input as file_input
+from glob import glob
+from os import rename
+from os.path import isfile
+from sys import exit
+
+TEMP_FILE_NAME = ".tmp"
+MERGED_FILE_NAME = "merged.vcf"
+
+def print_file_exists(filename):
+  print("A file named \"" + filename + "\" already exists in this folder!")
+
+if isfile(MERGED_FILE_NAME): exit(print_file_exists(MERGED_FILE_NAME))
+if isfile(TEMP_FILE_NAME): exit(print_file_exists(TEMP_FILE_NAME))
+
+contact_files = glob("*.vcf")
+
+if contact_files:
+  print("Merging contacts...")
+  with open(TEMP_FILE_NAME, 'wt') as merged_file, file_input(contact_files) as input_lines:
+    merged_file.writelines(input_lines)
+  rename(TEMP_FILE_NAME, MERGED_FILE_NAME)
+  print("Contacts successfully merged!")
+else:
+  print("No VCF files found in this folder!")
