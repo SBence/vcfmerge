be637047edf46f48301b19d5958938ae8a97825b
SBence <43538202+SBence@users.noreply.github.com>
2024-01-23 13:17:30 +0100

    Show merged file count, stop if only one file found

--- src/main.py
+++ src/main.py
@@ -8,18 +8,21 @@ TEMP_FILE_NAME = ".tmp"
 MERGED_FILE_NAME = "merged.vcf"
 
 def get_file_exists_string(filename):
-  return "A file named \"" + filename + "\" already exists in this folder!"
+  return "A file named \"" + filename + "\" already exists in this folder."
 
 if isfile(MERGED_FILE_NAME): exit(get_file_exists_string(MERGED_FILE_NAME))
 if isfile(TEMP_FILE_NAME): exit(get_file_exists_string(TEMP_FILE_NAME))
 
 contact_files = glob("*.vcf")
+contact_files_length = len(contact_files)
 
-if contact_files:
+if contact_files_length > 1:
   print("Merging contacts...")
   with open(TEMP_FILE_NAME, 'wt') as merged_file, file_input(contact_files) as input_lines:
     merged_file.writelines(input_lines)
   rename(TEMP_FILE_NAME, MERGED_FILE_NAME)
-  print("Contacts successfully merged!")
+  print(str(contact_files_length) + " contacts successfully merged.")
+elif contact_files_length == 1:
+  print("One VCF file found, nothing to merge.")
 else:
-  print("No VCF files found in this folder!")
+  print("No VCF files found in this folder.")
